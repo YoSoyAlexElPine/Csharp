3bd8844beacc5a9e794cb29ab224b5d7ccdd2980
YoSoyAlexElPine <machupichu0409@gmail.com>
2023-09-14 21:12:54 +0200

    Push automatico

--- Ejercicio_12/Ejercicio_12/Program.cs
+++ Ejercicio_12/Ejercicio_12/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numeroDecimal,numeroBinario;
+
+Console.Write("Introduce numero hexadecimal: ");
+string numeroHexadecimal = Console.ReadLine();
+Console.WriteLine();
+
+numeroDecimal = ParseDecimal(numero0);
+
+Console.WriteLine("Numero decimal: " + numeroDecimal);
+    
+;
+
+static int ParseDecimal(string numero0)
+{
+    int contador = numero0.Length;
+    int sumatorio = 0;
+    char[] array = numero0.ToCharArray();
+    foreach (char item in array)
+    {
+        if (item == '1')
+        {
+            sumatorio = sumatorio + ((int)Math.Pow(2, contador - 1));
+        }
+
+        contador--;
+    }
+    return sumatorio;
+}
+
+static bool VerificadorHexa(string cadena)
+{
+    char[] array = cadena.ToCharArray();    
+    char[] arrayVerif = { 'A', 'B', 'C', 'D', 'E', 'F', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+    foreach (char item in array)
+    {
+        if (array.Contains(arrayVerif))
+        {
+            
+        }
+    }
+
+
+}
