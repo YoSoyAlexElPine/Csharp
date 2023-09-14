da9f21ffb6df7a28a41eb4047899bfd322cb68ad
YoSoyAlexElPine <machupichu0409@gmail.com>
2023-09-14 17:06:13 +0200

    Cambio desde Visual

--- Ejercicio_5/Ejercicio_5/Program.cs
+++ Ejercicio_5/Ejercicio_5/Program.cs
@@ -1,11 +1,10 @@
-﻿
-int numero = 500000000;
+﻿int numero = 50;
 double suma1 = 0;
 double suma2 = 0;
-bool conmutador=true;
+bool conmutador = true;
 double division;
 
-for (int i = 1; i < numero + 1; i+=2)
+for (int i = 1; i < numero + 1; i += 2)
 {
     division = (double)1 / i;
 
@@ -13,17 +12,49 @@ for (int i = 1; i < numero + 1; i+=2)
     {
         suma1 = suma1 - division;
         conmutador = false;
-    } else
+    }
+    else
     {
         suma1 = suma1 + division;
         conmutador = true;
     }
-
-
 }
 
+for (int i = 1; i < numero + 1; i += 2)
+{
+
+    //Console.WriteLine(CalcularFactorial(i));
+    //Console.WriteLine(CalcularFactorialImpar(i));
 
 
-Console.WriteLine("Suma del enunciado: {0}", 4*suma1);
+    division = (double)CalcularFactorial(i) / (double)CalcularFactorialImpar(i);
+
+    suma2 = suma2 + division;
+}
+
+Console.WriteLine("Suma del enunciado: {0}", Math.Abs(4 * suma1));
 Console.WriteLine("Suma correcta: {0}", suma2);
 Console.WriteLine("Diferencia: {0}", suma2 - suma1);
+
+static double CalcularFactorial(int n)
+{
+    double suma = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        suma *= i;
+    }
+    return suma;
+}
+
+static double CalcularFactorialImpar(int n)
+{
+    double suma = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0) // Corrección: Verificar si i es impar
+        {
+            suma *= i;
+        }
+    }
+    return suma;
+}
